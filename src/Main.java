c5067401e4d2553c52036121a455d05ced31cfff
Alena Zakirova <grachevaalek@gmail.com>
2023-09-25 16:30:05 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.security.spec.RSAOtherPrimeInfo;
+
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 55_676;
+        int rub = 20;
+
+        int mile = ticket / rub;
+        System.out.println(mile);
+    }
+}
